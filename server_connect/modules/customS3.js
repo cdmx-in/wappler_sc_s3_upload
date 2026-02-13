df3de0627977f2af26fbc0e3eefd8e994e5fa1f1
Swapnil Bhosale <swapnil.bhosale@cdmx.in>
2026-02-13 16:46:09 +0530

    Updated extension to include Listing, Copying, and deleting files in S3

--- server_connect/modules/customS3.js
+++ server_connect/modules/customS3.js
@@ -1,207 +1,159 @@
 const fs = require('fs-extra');
-const { S3 } = require('@aws-sdk/client-s3');
+const { S3, PutObjectCommand, GetObjectCommand, ListObjectsCommand, CopyObjectCommand, DeleteObjectCommand } = require('@aws-sdk/client-s3');
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
-const { PutObjectCommand, GetObjectCommand } = require('@aws-sdk/client-s3');
 const mime = require('mime-types');
 const path = require('path');
 
-exports.s3_signed_upload = async function (options) {
-    const Bucket = this.parseRequired(options.bucket, 'string', 'Bucket is required.');
-    const Provider = this.parseOptional(options.provider, 'string', 'aws');
-    const Key = this.parseRequired(options.key, 'string', 'Key is required.');
-    const ContentType = this.parseOptional(options.contentType, 'string', mime.lookup(Key) || 'application/octet-stream');
-    const expiresIn = this.parseOptional(options.expires, 'number', 300);
-    const ACL = this.parseOptional(options.acl, 'string', undefined);
-    const accessKeyId = this.parseRequired(options.accessKeyId, 'string', 'AccessKeyId is required.');
-    const secretAccessKey = this.parseRequired(options.secretAccessKey, 'string', 'SecretAccessKey is required.');
-    const region = this.parseOptional(options.region, 'string', 'us-east-1');
-    let endpoint = `https://s3.${region}.amazonaws.com`;
-    const forcePathStyle = this.parseOptional(options.forcePathStyle, 'boolean', false);
-    let config = { endpoint: endpoint, credentials: { accessKeyId, secretAccessKey }, region, signatureVersion: 'v4', forcePathStyle: forcePathStyle };
-    if (Provider === "custom" && options.endpoint != "" && options.endpoint != "undefined") {
-        config.endpoint = options.endpoint;
+function buildS3Config(options, ctx) {
+    const accessKeyId = ctx.parseRequired(options.accessKeyId, 'string', 'AccessKeyId is required.');
+    const secretAccessKey = ctx.parseRequired(options.secretAccessKey, 'string', 'SecretAccessKey is required.');
+    const region = ctx.parseOptional(options.region, 'string', 'us-east-1');
+    const provider = ctx.parseOptional(options.provider, 'string', 'aws');
+    const endpointOption = ctx.parseOptional(options.endpoint, 'string', '');
+    const forcePathStyle = ctx.parseOptional(options.forcePathStyle, 'boolean', false);
+
+    const config = {
+        region,
+        credentials: { accessKeyId, secretAccessKey },
+        forcePathStyle,
+        endpoint: `https://s3.${region}.amazonaws.com`
+    };
+
+    if (provider === "custom" && endpointOption) {
+        config.endpoint = endpointOption;
     }
-    const s3 = await getS3Object(options, this);
-    const command = new PutObjectCommand({ Bucket, Key, ContentType, ACL });
-    return getSignedUrl(s3, command, { expiresIn });
+
+    return { config, region, provider, forcePathStyle };
 }
+
+function getS3Client(options, ctx) {
+  const { config } = buildS3Config(options, ctx);
+  return new S3(config);
+}
+
+function buildFileUrl({ endpoint, bucket, key, region, provider, forcePathStyle }) {
+  if (forcePathStyle || provider === "custom") {
+      return `${endpoint.replace(/\/$/, '')}/${bucket}/${encodeURIComponent(key)}`;
+  }
+  return `https://${bucket}.s3.${region}.amazonaws.com/${encodeURIComponent(key)}`;
+}
+
+exports.s3_signed_upload = async function (options) {
+  const Bucket = this.parseRequired(options.bucket, 'string', 'Bucket is required.');
+  const Key = this.parseRequired(options.key, 'string', 'Key is required.');
+  const ContentType = this.parseOptional(options.contentType, 'string', mime.lookup(Key) || 'application/octet-stream');
+  const expiresIn = this.parseOptional(options.expires, 'number', 300);
+  const ACL = this.parseOptional(options.acl, 'string',undefined);
+
+  const s3 = getS3Client(options, this);
+  const command = new PutObjectCommand({ Bucket, Key, ContentType, ACL });
+
+  return getSignedUrl(s3, command, { expiresIn });
+};
 
 exports.s3_signed_download = async function (options) {
-    const Bucket = this.parseRequired(options.bucket, 'string', 'Bucket is required.');
-    const Provider = this.parseOptional(options.provider, 'string', 'aws');
-    const Key = this.parseRequired(options.key, 'string', 'Key is required.');
-    const expiresIn = this.parseOptional(options.expires, 'number', 300);
-    const accessKeyId = this.parseRequired(options.accessKeyId, 'string', 'AccessKeyId is required.');
-    const secretAccessKey = this.parseRequired(options.secretAccessKey, 'string', 'SecretAccessKey is required.');
-    const region = this.parseOptional(options.region, 'string', 'us-east-1');
-    let endpoint = `https://s3.${region}.amazonaws.com`;
-    const forcePathStyle = this.parseOptional(options.forcePathStyle, 'boolean', false);
-    let config = { endpoint: endpoint, credentials: { accessKeyId, secretAccessKey }, region, signatureVersion: 'v4', forcePathStyle: forcePathStyle };
-    if (Provider === "custom" && options.endpoint != "" && options.endpoint != "undefined") {
-        config.endpoint = options.endpoint;
-    }
-    const s3 = await getS3Object(options, this);
-    const command = new GetObjectCommand({ Bucket, Key });
-    return getSignedUrl(s3, command, { expiresIn });
-}
+  const Bucket = this.parseRequired(options.bucket, 'string', 'Bucket is required.');
+  const Key = this.parseRequired(options.key, 'string', 'Key is required.');
+  const expiresIn = this.parseOptional(options.expires, 'number', 300);
+
+  const s3 = getS3Client(options, this);
+  const command = new GetObjectCommand({ Bucket, Key });
+
+  return getSignedUrl(s3, command, { expiresIn });
+};
 
 exports.s3_put_object = async function (options) {
-    const File = this.parseRequired(options.file, 'string', 'File input name is required.');
-    const Bucket = this.parseRequired(options.bucket, 'string', 'Bucket is required.');
-    const Provider = this.parseOptional(options.provider, 'string', 'aws');
-    const Key = this.parseRequired(options.key, 'string', 'Key is required.');
-    const ContentType = this.parseOptional(options.contentType, 'string', mime.lookup(Key) || 'application/octet-stream');
-    const ACL = this.parseOptional(options.acl, 'string', undefined);
-    const accessKeyId = this.parseRequired(options.accessKeyId, 'string', 'AccessKeyId is required.');
-    const secretAccessKey = this.parseRequired(options.secretAccessKey, 'string', 'SecretAccessKey is required.');
-    const region = this.parseOptional(options.region, 'string', 'us-east-1');
-    const ContentDisposition = this.parseOptional(options.contentDisposition, 'string', undefined);
-    const endpoint = `https://s3.${region}.amazonaws.com`;
-    const forcePathStyle = this.parseOptional(options.forcePathStyle, 'boolean', false);
-    const useFilePath = this.parseOptional(options.useFilePath, 'boolean', false);
-    let config = { endpoint: endpoint, credentials: { accessKeyId, secretAccessKey }, region, signatureVersion: 'v4', forcePathStyle: forcePathStyle };
-    if (Provider === "custom" && options.endpoint != "" && options.endpoint != "undefined") {
-        config.endpoint = options.endpoint;
-    }
-    const s3 = await getS3Object(options, this);
+  const File = this.parseRequired(options.file, 'string');
+  const Bucket = this.parseRequired(options.bucket, 'string', 'Bucket is required.');
+  const Key = this.parseRequired(options.key, 'string', 'Key is required.');
+  const ContentType = this.parseOptional(options.contentType, 'string', mime.lookup(Key) || 'application/octet-stream');
+  const ACL = this.parseOptional(options.acl, 'string');
+  const ContentDisposition = this.parseOptional(options.contentDisposition, 'string');
+  const useFilePath = this.parseOptional(options.useFilePath, 'boolean', false);
 
-    if (!s3) throw new Error(`S3 provider "${provider}" doesn't exist.`);
+  const { config, region, provider, forcePathStyle } = buildS3Config(options, this);
+  const s3 = new S3(config);
 
-    let filePath = path.join(process.cwd(), File); //File;
+  let filePath = useFilePath
+      ? path.join(process.cwd(), File)
+      : this.req.files[File].tempFilePath;
 
-    if (!useFilePath) {
-        const ReqFile = this.req.files[File];
-        filePath = ReqFile.tempFilePath;
-    }
+  const Body = fs.createReadStream(filePath);
 
+  const command = new PutObjectCommand({
+      Bucket,
+      Key,
+      ContentType,
+      ACL,
+      ContentDisposition,
+      Body
+  });
 
-    let Body = fs.createReadStream(filePath);
+  const result = await s3.send(command);
 
+  return {
+      ...result,
+      url: buildFileUrl({
+          endpoint: config.endpoint,
+          bucket: Bucket,
+          key: Key,
+          region,
+          provider,
+          forcePathStyle
+      }),
+      bucket: Bucket,
+      key: Key
+  };
+};
 
-    const command = new PutObjectCommand({ Bucket, Key, ContentType, ACL, ContentDisposition, Body });
-    try {
+exports.s3_list_files = async function (options) {
+  const Bucket = this.parseRequired(options.bucket, 'string', 'Bucket is required.');
+  const Prefix = this.parseOptional(options.prefix, 'string', '');
 
-        let result = await s3.send(command);
-        let url;
-        if (forcePathStyle || Provider === "custom") {
-            // path-style or custom endpoint: https://endpoint/bucket/key
-            url = `${endpoint.replace(/\/$/, '')}/${Bucket}/${encodeURIComponent(Key)}`;
-        } else {
-            // virtual-hosted–style: https://bucket.s3.region.amazonaws.com/key
-            url = `https://${Bucket}.s3.${region}.amazonaws.com/${encodeURIComponent(Key)}`;
-        }
+  const { config, region, provider, forcePathStyle } = buildS3Config(options, this);
+  const s3 = new S3(config);
 
-        return {
-            ...result,
-            url,
-            bucket: Bucket,
-            key: Key
-        };
-    } catch (error) {
-        throw new Error(`Failed to upload file to S3: ${error}`);
-    }
-}
+  const result = await s3.send(new ListObjectsCommand({ Bucket, Prefix }));
+  if (!result.Contents) return [];
 
-exports.s3_put_objects = async function (options) {
-    const Files = this.parseRequired(options.files, 'array', 'Files input array is required.');
-    const Bucket = this.parseRequired(options.bucket, 'string', 'Bucket is required.');
-    const Provider = this.parseOptional(options.provider, 'string', 'aws');
-    const baseKey = this.parseOptional(options.baseKey, 'string', '');
-    const keyPattern = this.parseOptional(options.keyPattern, 'string', 'filename');
-    const ACL = this.parseOptional(options.acl, 'string', undefined);
-    const accessKeyId = this.parseRequired(options.accessKeyId, 'string', 'AccessKeyId is required.');
-    const secretAccessKey = this.parseRequired(options.secretAccessKey, 'string', 'SecretAccessKey is required.');
-    const region = this.parseOptional(options.region, 'string', 'us-east-1');
-    const endpoint = `https://s3.${region}.amazonaws.com`;
-    const forcePathStyle = this.parseOptional(options.forcePathStyle, 'boolean', false);
-    const useFilePath = this.parseOptional(options.useFilePath, 'boolean', false);
+  return result.Contents.map(file => ({
+      key: file.Key,
+      size: file.Size,
+      lastModified: file.LastModified,
+      etag: file.ETag,
+      url: buildFileUrl({
+          endpoint: config.endpoint,
+          bucket: Bucket,
+          key: file.Key,
+          region,
+          provider,
+          forcePathStyle
+      })
+  }));
+};
 
-    let config = { endpoint: endpoint, credentials: { accessKeyId, secretAccessKey }, region, signatureVersion: 'v4', forcePathStyle: forcePathStyle };
-    if (Provider === "custom" && options.endpoint != "" && options.endpoint != "undefined") {
-        config.endpoint = options.endpoint;
-    }
+exports.s3_copy_object = async function (options) {
+  const SrcBucket = this.parseRequired(options.srcBucket, 'string', 'Source Bucket is required.');
+  const SrcKey = this.parseRequired(options.srcKey, 'string', 'Source Key is required.');
+  const DstBucket = this.parseRequired(options.dstBucket, 'string', 'Destination Bucket is required.');
+  const DstKey = this.parseRequired(options.dstKey, 'string', 'Destination Key is required.');
 
-    const s3 = await getS3Object(options, this);
-    if (!s3) throw new Error(`S3 provider "${Provider}" doesn't exist.`);
+  const s3 = getS3Client(options, this);
 
-    try {
-        let counter = 0;
-        const timestamp = Date.now();
+  return s3.send(new CopyObjectCommand({
+      Bucket: DstBucket,
+      Key: DstKey,
+      CopySource: `${SrcBucket}/${SrcKey}`
+  }));
+};
 
-        const uploadResults = await Promise.all(Files.map(async (fileInfo) => {
-            let Key;
-            const filename = path.basename(fileInfo.file);
+exports.s3_delete_file = async function (options) {
+  const Bucket = this.parseRequired(options.bucket, 'string', 'Bucket is required.');
+  const Key = this.parseRequired(options.key, 'string', 'Key is required.');
 
-            // Generate key based on pattern
-            switch(keyPattern) {
-                case 'increment':
-                    counter++;
-                    Key = `${baseKey}${counter}`;
-                    break;
-                case 'timestamp':
-                    Key = `${baseKey}${timestamp}`;
-                    break;
-                case 'filename':
-                default:
-                    Key = `${baseKey}${filename}`;
-            }
+  const s3 = getS3Client(options, this);
 
-            // Ensure no double slashes in key
-            Key = Key.replace(/\/+/g, '/');
-            // Remove leading slash if present
-            Key = Key.replace(/^\//, '');
+  await s3.send(new DeleteObjectCommand({ Bucket, Key }));
 
-            const ContentType = fileInfo.contentType || mime.lookup(Key) || 'application/octet-stream';
-            const ContentDisposition = fileInfo.contentDisposition;
-
-            let filePath = useFilePath ? path.join(process.cwd(), fileInfo.file) : this.req.files[fileInfo.file].tempFilePath;
-            let Body = fs.createReadStream(filePath);
-
-            const command = new PutObjectCommand({
-                Bucket,
-                Key,
-                ContentType,
-                ACL,
-                ContentDisposition,
-                Body
-            });
-
-            const result = await s3.send(command);
-
-            let url;
-            if (forcePathStyle || Provider === "custom") {
-                url = `${endpoint.replace(/\/$/, '')}/${Bucket}/${encodeURIComponent(Key)}`;
-            } else {
-                url = `https://${Bucket}.s3.${region}.amazonaws.com/${encodeURIComponent(Key)}`;
-            }
-
-            return {
-                ...result,
-                url,
-                bucket: Bucket,
-                key: Key,
-                originalFile: fileInfo.file
-            };
-        }));
-
-        return {
-            success: true,
-            results: uploadResults
-        };
-    } catch (error) {
-        throw new Error(`Failed to upload files to S3: ${error}`);
-    }
-}
-
-const getS3Object = async function (options, thisKeyword) {
-    const accessKeyId = thisKeyword.parseRequired(options.accessKeyId, 'string', 'AccessKeyId is required.');
-    const secretAccessKey = thisKeyword.parseRequired(options.secretAccessKey, 'string', 'SecretAccessKey is required.');
-    const region = thisKeyword.parseOptional(options.region, 'string', 'us-east-1');
-    const forcePathStyle = thisKeyword.parseOptional(options.forcePathStyle, 'boolean', false);
-    let endpoint = `https://s3.${region}.amazonaws.com`;
-    let config = { endpoint: endpoint, credentials: { accessKeyId, secretAccessKey }, region, signatureVersion: 'v4', forcePathStyle: forcePathStyle };
-    if (options.provider === "custom" && options.endpoint != "" && options.endpoint != "undefined") {
-        config.endpoint = options.endpoint;
-    }
-    return new S3(config);
-}
+  return { success: true, bucket: Bucket, key: Key };
+};
